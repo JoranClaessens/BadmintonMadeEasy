35d35d06b71b660efc72e8a52f9eb6a8fb92351c
Joran Claessens <11400919@student.pxl.be>
2018-02-09 13:39:26 +0100

    Bootstrap navbar implemented

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   websiteTitle = 'Badminton Made Easy';
+  websiteIcon = 'assets/img/badminton-shuttle-icon1.png';
 
   constructor() { }
 
